e73e4638c70b97ac97ba30e46a92b9fdfc864685
Manling (Jenny) Zhao <jennyzhao0711@gmail.com>
2025-01-17 15:15:09 -0800

    data models defined

---
+++ server/models/VisaManagementModel.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const visaManagementSchema = new Schema({
+  employeeId: { 
+    type: Schema.Types.ObjectId, 
+    ref: "Employee", 
+    required: true 
+  },
+  status: { 
+    type: String, 
+    required: true 
+  },
+  currentStep: { 
+    type: String,
+    required: true 
+  },
+  steps: [
+    {
+      type: { 
+        type: String, 
+        required: true 
+      },
+      status: { 
+        type: String, 
+        required: true 
+      },
+      feedback: { 
+        type: String 
+      },
+      documents: [
+        {
+          fileUrl: { 
+            type: String, 
+            required: true 
+          },
+          createdAt: { 
+            type: Date, 
+            default: Date.now 
+          },
+        },
+      ],
+    },
+  ],
+});
+
+module.exports = mongoose.model("VisaManagement", visaManagementSchema);
