30b156593084993329c5a37068bb8b71e8491be2
Manling (Jenny) Zhao <jennyzhao0711@gmail.com>
2025-01-18 21:31:49 -0800

    remove unused files

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "../features/auth/authSlice";
-import userSlice from "../features/users/userSlice";
 
 const store = configureStore({
   reducer: {
     auth: authSlice,
-    user: userSlice,
   },
 });
 
